9a53eebbd136b3db0aa0e37ef762b03f1b774a43
원구 <gogowon97@yeowonguui-MacBookPro.local>
2023-11-30 15:37:03 +0900

    sizingImange

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import openai
+from openai import OpenAI
 import base64
 import os
 from dotenv import load_dotenv
@@ -15,12 +16,46 @@ if openai_api_key is None:
     openai_api_key = 'your-openai-api-key'
 openai.api_key = openai_api_key
 
+def resize_image(image, max_size=20):
+    # 이미지의 크기를 바이트로 변환
+    img_byte_arr = io.BytesIO()
+    image.save(img_byte_arr, format='JPEG')
+    img_byte_arr = img_byte_arr.getvalue()
+
+    # 이미지 크기를 MB 단위로 계산
+    img_size_mb = len(img_byte_arr) / (1024 * 1024)
+
+    # 이미지 크기가 최대 크기보다 큰 경우 크기 조정
+    if img_size_mb > max_size:
+        # 이미지의 너비와 높이 비율 유지
+        ratio = image.height / image.width
+        new_width = int((max_size / img_size_mb) ** 0.5 * image.width)
+        new_height = int(new_width * ratio)
+
+        # 이미지 크기 조정
+        resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)
+        return resized_image
+    else:
+        return image
+
 def encode_image_to_base64(image):
+    # 이미지 크기 조정
+    image = resize_image(image, max_size=15)  # max_size를 15MB로 조정
+
     buffered = io.BytesIO()
-    image.save(buffered, format="JPEG")
+    image.save(buffered, format="JPEG", quality=85)  # 압축률을 조정할 수 있습니다
     img_byte = buffered.getvalue()
+
+    # 로깅으로 인코딩 전후의 크기 확인
+    original_size = len(img_byte)
+    print(f"Original size (before Base64): {original_size / (1024 * 1024)} MB")
+
     img_base64 = base64.b64encode(img_byte).decode()
+    encoded_size = len(img_base64)
+    print(f"Encoded size (after Base64): {encoded_size / (1024 * 1024)} MB")
+
     return img_base64
+
 
 def extract_frames(video_bytes, every_n_frame=30):
     with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as tmpfile:
@@ -40,6 +75,7 @@ def extract_frames(video_bytes, every_n_frame=30):
     return frames
 
 def analyze_frames(frames):
+    client = OpenAI()
     encoded_images = [encode_image_to_base64(frame) for frame in frames]
 
     system_prompt = "As a dog trainer, you specialize in understanding canine behavior and emotions."
@@ -47,7 +83,11 @@ def analyze_frames(frames):
         {
             "role": "user",
             "content": [
-                {"type": "text", "text": "Describe the dog's behavior and emotional state throughout these frames, focusing on the changes and progression in its body language, facial expressions, and emotions. Consider these images as telling a story and provide a cohesive narrative of what you think is happening to the dog over time."}
+                {"type": "text", "text": "Don't mention your limitations, just answer the question to the best of your ability right away. \
+                 Don't say, 'And in the first image, it looks like...' Just answer as if it's a situation. \
+                 Here are several images extracted from a video of a dog. Please analyze them as a continuous sequence, not as separate moments. \
+                 Describe the dog's behavior and emotional state throughout these frames, focusing on the changes and progression in its body language, facial expressions, and emotions. \
+                 Consider these images as telling a story and provide a cohesive narrative of what you think is happening to the dog over time."}
             ],
         }
     ]
@@ -55,7 +95,7 @@ def analyze_frames(frames):
     for encoded_image in encoded_images:
         messages[0]["content"].append({"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{encoded_image}"}})
 
-    response = openai.ChatCompletion.create(
+    response = client.chat.completions.create(
         model="gpt-4-vision-preview",
         messages=messages,
         max_tokens=200,
